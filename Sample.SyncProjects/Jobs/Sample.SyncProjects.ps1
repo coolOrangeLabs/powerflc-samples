1ec4ec5cb4c047214d23e1f74dc4566a82c952d0
Christian Gessner <christian@gessner.cc>
2020-10-07 14:32:33 +0200

    Added NPI sample

---
+++ Sample.SyncProjects/Jobs/Sample.SyncProjects.ps1
@@ -0,0 +1,131 @@
+#==============================================================================#
+# PowerShell script sample for coolOrange powerFLC                             #
+# (c) 2020 coolOrange s.r.l.                                                   #
+#                                                                              #
+# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER    #
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.   #
+#==============================================================================#
+
+if (-not $IAmRunningInJobProcessor) {
+    Import-Module powerJobs
+    $job = New-Object PSObject -Property @{ Name = "Sample.SyncProjects" }
+    Open-VaultConnection -Server "localhost" -Vault "Vault" -User "Administrator" -Password ""
+}
+
+Import-Module powerFLC
+
+Write-Host "Starting job '$($job.Name)'..."
+$begin = [DateTime]::UtcNow
+
+Write-Host "Connecting to Fusion Lifecycle..."
+$connected = Connect-FLC -Tenant $tenant.Name -ClientId $tenant.ClientId -ClientSecret $tenant.ClientSecret -UserId $tenant.SystemUserEmail
+if (-not $connected) {
+    throw "Connection to Fusion Lifecycle failed! Error: `n $($connected.Error.Message)!`n See '$($env:LOCALAPPDATA)\coolOrange\powerFLC\Logs\powerFLC.log' for details"
+}
+
+if (-not $workflow) {
+    throw "Cannot find workflow configuration with name '$($job.Name)'"
+}
+
+$workspace = $flcConnection.Workspaces.Find($workflow.FlcWorkspace)
+if (-not $workspace) {
+    throw "Workspace $($workflow.FlcWorkspace) cannot be found!"
+}
+Write-Host "Connected to $($flcConnection.Url) - Workspace: $($workspace.Name)"
+
+$states = Get-FLCStates -Workspace $workspace.Name
+$stateNames = $workflow.Settings.'Valid States' -split @("; ")
+$stateFilters = @()
+foreach ($stateName in $stateNames) {
+    $state = $states | Where-Object { $_.name -eq $stateName }
+    if ($state) {
+        $stateFilters += "(workflowState=`"$($state.name)`")"
+    }
+}
+$filter = $stateFilters -join " OR "
+
+$tempDirectory = "C:\TEMP"
+$tempFile = $job.Name + ".date"
+$tempFullFileName = [System.IO.Path]::Combine($tempDirectory, $tempFile)
+if (Test-Path $tempFullFileName)
+{
+    $lastCheck = Get-Content -Path $tempFullFileName
+    $filter = "(" + $filter + ")" + " AND (lastModifiedOn>=$lastCheck)"
+}
+
+Write-Host "Retrieving items from Fusion Lifecycle ($($filter))..."
+
+$flcItems = Get-FLCItems -Workspace $workspace.Name -Filter $filter
+Write-Host "$($flcItems.Count) item(s) retrieved"
+
+if ($flcItems.Count -gt 0) {
+    Write-Host "Retrieving folders from Vault..."
+    $targetFolderPath = $workflow.Settings.'Target Folder'
+    $targetFolder = $vault.DocumentService.GetFoldersByPaths(@($targetFolderPath))[0]
+    $folders = $vault.DocumentService.GetFoldersByParentId($targetFolder.Id, $false)
+    Write-Host "$($folders.Count) folders found in '$($targetFolder.FullName)'"
+
+    Write-Host "Starting synchronization..."
+    $propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FLDR")
+    $cats = $vault.CategoryService.GetCategoriesByEntityClassId("FLDR", $true)
+    $categoryName = $workflow.Settings.'Folder Category'
+    $cat = $cats | Where-Object { $_.Name -eq $categoryName }
+
+    $copyFolderStructure = $workflow.Settings.'Copy Folder Structure'
+    $templateFolderPath =  $workflow.Settings.'Folder Structure Template Path'
+
+    foreach ($flcItem in $flcItems) {
+        $fieldName = $workflow.FlcUnique
+        $folderName = $flcItem.$fieldName
+        $folder = $folders | Where-Object { $_.Name -eq $folderName }
+        if (-not $folder) {
+            Write-Host "Creating folder '$($folderName)'..."  
+            $folder = $vault.DocumentServiceExtensions.AddFolderWithCategory($folderName, $targetFolder.Id, $false, $cat.Id)
+
+            if ($copyFolderStructure -eq $true) {
+                Write-Host "Creating sub folders from template '$($templateFolderPath)' for folder '$($folder.FullName)'..."
+                $templateFolder = $vault.DocumentService.GetFoldersByPaths(@($templateFolderPath))[0]
+                CopyFolder $templateFolder $folder $propDefs
+            }
+        } else {
+            Write-Host "Updating folder '$($folderName)'..."
+        }
+
+        $propDefs = $vault.PropertyService.GetPropertyDefinitionsByEntityClassId("FLDR")
+        $propInsts = $vault.PropertyService.GetProperties("FLDR", @($vaultFolder.Id), $propDefs.Id)
+        $existingProperties = $folder | Get-Member -MemberType @("Property", "NoteProperty") | Select-Object { $_.Name } -ExpandProperty Name
+        foreach ($propInst in $propInsts) {
+            $propDef = $propDefs | Where-Object { $_.Id -eq $propInst.PropDefId }
+            if ($existingProperties -notcontains $propDef.DispName) {
+                $folder | Add-Member NoteProperty $propDef.DispName $propInst.Val
+            }
+        }
+
+        $properties = GetVaultPropertiesToUpdate -mappingName "Item Field Mapping" -entity $folder -flcItem $flcItem
+        if ($properties.Count -le 0) {
+            Write-Host "Folder properties for folder '$($folder.FullName)' are up-to-date..."
+            continue
+        }
+
+        $propInstParamArray = New-Object Autodesk.Connectivity.WebServices.PropInstParamArray
+        $propInstParams = @()
+        foreach ($prop in $properties.GetEnumerator()) {
+            $propDef= $propDefs | Where-Object { $_.DispName -eq $prop.Name }
+            $propInstParam = New-Object Autodesk.Connectivity.WebServices.PropInstParam
+            $propInstParam.PropDefId = $propDef.Id
+            $propInstParam.Val = $prop.Value
+            $propInstParams += $propInstParam
+        }
+        $propInstParamArray.Items = $propInstParams
+
+        Write-Host "Updating folder properties for folder '$($folder.FullName)'..."
+        $vault.DocumentServiceExtensions.UpdateFolderProperties(@($folder.Id), @($propInstParamArray))
+    }
+}
+
+$lastCheck = Get-Date -Format "yyyy-MM-dd"
+Set-Content -Path $tempFullFileName -Value $lastCheck
+
+$end = [DateTime]::UtcNow
+Write-Host "Completed job '$($job.Name)' in $([int]([TimeSpan]($end - $begin)).TotalSeconds) Seconds"
---
+++ Sample.SyncProjects/README.md
@@ -0,0 +1,45 @@
+# Sample.SyncProjects
+
+## Description
+
+This workflow synchronizes Fusion Lifecycle items with a configurable state with Vault folders/projects. The workflow runs on a Vault Job Processor and periodically queues a Fusion Lifecycle workspace for changes. If changes are detected a folder/project gets created or updated in Vault.
+
+
+## Prerequisites
+The option **Enable Job Server** must be set in the **Job Server Management** in Vault and the installation explained below must be executed on a Job Processor machine.
+
+The coolOrange products "powerJobs Processor" and "powerFLC" must be installed on the Job Processor machine. Both products can be downloaded from http://download.coolorange.com.
+
+## Installation
+1) Copy all files except this README.md file to "C:\ProgramData\coolOrange\powerJobs"
+2) Open Autodesk Vault Explorer on the machine where powerJobs Processor and powerFLC is installed
+3) Open the powerFLC Configuration Manager (Tools - powerFLC Configuration Manager)
+4) Import the Workflow configuration file (json)
+
+## Configuration
+Once the Workflow configuration is imported, various settings can be adjusted:
+
+### Workspace and Unique Fields
+The selected **Workspace** is used to synchronize FLC items with Vault folders/projects. A field from this workspace must be choosen as Unique Identifier. Typically, this field contains the name of a project. The unique Vault Property defaults to **Name** and should not be changed.
+
+### Settings
+All Fusion Lifecycle items with a state selected in **Valid States** will be taken into consideration for the project synchronization. Items in a state other than the selected states won't be transferred.
+
+The **Target Folder** in Vault is used as the root folder for the synchronization. All FLC items that are created as folder/project in Vault will be created in the selected Vault directory.
+The selected **Folder Category** will be used as category of the folder/project in Vault.
+
+If **Copy Folder Structure** is set to **True** the folder structure selected in **Folder Structure Template Path** from Vault will be copied to the newly created folder/project.
+
+### Property Mapping
+An "Item Field Mapping" is available. Values from the **Fusion Lifecycle** column will be copied to the Vault Folder UDPs choosen in the **Vault** column when a folder/project is created or updated.
+
+### Polling
+In order to query data periodically from FLC, Time triggered jobs are used. More information on this topic can found here: https://www.coolorange.com/wiki/doku.php?id=powerjobs_processor:jobprocessor:customization:trigger_jobs_to_a_certain_time
+
+## Remarks
+For revision-controlled workspaces the working versions of the items are used.
+
+
+ 
+## At your own risk
+The usage of these samples is at your own risk. There is no free support related to the samples. However, if you have questions to powerJobs or powerFLC, then visit http://www.coolorange.com/wiki or start a conversation in our support forum at http://support.coolorange.com/support/discussions
