c072f37b04ea16ecaddf61b972eacb06cd299cba
Christian Gessner <christian@gessner.cc>
2020-12-08 17:43:39 +0100

    Added File Publishing Workflow

---
+++ coolorange.flc.transfer.file/Jobs/coolorange.flc.transfer.file.ps1
@@ -0,0 +1,120 @@
+#==============================================================================#
+# PowerShell script sample for coolOrange powerFLC                             #
+# (c) 2020 coolOrange s.r.l.                                                   #
+#                                                                              #
+# THIS SCRIPT/CODE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER    #
+# EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES  #
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.   #
+#==============================================================================#
+
+Import-Module powerFLC
+
+Write-Host "Starting job '$($job.Name)'..."
+$begin = [DateTime]::UtcNow
+
+if (-not $IAmRunningInJobProcessor) {
+    Import-Module powerJobs
+    $job = New-Object PSObject -Property @{ Name = "Sample.TransferFiles" }
+    Open-VaultConnection -Server "localhost" -Vault "Vault" -User "Administrator" -Password ""
+    $file = Get-VaultFile -File "$/Designs/Inventor Sample Data/Models/Assemblies/Stapler/Stapler.idw"
+    $user = $vault.AdminService.GetUserByUserId($vaultConnection.UserID)
+} else {
+    $jobs = $vault.JobService.GetJobsByDate([int]::MaxValue, [DateTime]::MinValue)
+    $user = $vault.AdminService.GetUserByUserId(($jobs | Where-Object { $_.Id -eq $job.Id }).CreateUserId)    
+}
+
+$supportedExtensions = $workflow.Settings.'Supported File Extensions'.Split(@(';', ' '), [System.StringSplitOptions]::RemoveEmptyEntries)
+if ($supportedExtensions -notcontains $file._Extension) {
+    Write-Host "Files with extension: '$($file._Extension)' are not supported"
+    return
+}
+
+Write-Host "User: '$($user.Name)', Email: '$($user.Email)'"
+if (-not $user.Email) {
+    throw "There is no email address configured for user '$($user.Name)'! The Vault user email address is used to authenticate with Fusion Lifecycle!"
+}
+
+Write-Host "Connecting to Fusion Lifecycle..."
+$connected = Connect-FLC -Tenant $tenant.Name -ClientId $tenant.ClientId -ClientSecret $tenant.ClientSecret -UserId $user.Email
+if (-not $connected) {
+    throw "Connection to Fusion Lifecycle failed! Error: `n $($connected.Error.Message)!`n See '$($env:LOCALAPPDATA)\coolOrange\powerFLC\Logs\powerFLC.log' for details"
+}
+
+if (-not $workflow) {
+    throw "Cannot find workflow configuration with name '$($job.Name)'"
+} 
+
+$workspace = $flcConnection.Workspaces.Find($workflow.FlcWorkspace)
+if (-not $workspace) {
+    throw "Workspace $($workflow.FlcWorkspace) cannot be found!"
+}
+Write-Host "Connected to $($flcConnection.Url) - Workspace: $($workspace.Name)"
+
+function CreateOrUpdateFLCItem($entity, $properties) {  
+    $uniqueFlcField = $workspace.ItemFields.Find($workflow.FlcUnique)
+    $flcItem = (Get-FLCItems -Workspace $workspace.Name -Filter ('ITEM_DETAILS:{0}="{1}"' -f $uniqueFlcField.Id, $entity."$($workflow.VaultUnique)"))[0]
+    if (-not $flcItem) {
+        Write-Host "Create item $($entity."$($workflow.VaultUnique)")..."
+        $flcItem = Add-FLCItem -Workspace $workspace.Name -Properties $properties
+    } else {
+        Write-Host "Update item $($entity."$($workflow.VaultUnique)")..."
+        $flcItem = Update-FLCItem -Workspace $flcItem.Workspace -ItemId $flcItem.Id -Properties $properties
+    }
+
+    if (-not $flcItem -or -not $flcItem.Id) {
+        throw "Item cannot be created/updated in Fusion Lifecycle"
+    } else {
+        $urn = "urn:adsk.plm:tenant.workspace.item:$($tenant.Name.ToUpper()).$($workspace.Id).$($flcItem.Id)"
+        $vault.PropertyService.SetEntityAttribute($entity.MasterId, "FLC.ITEM", "Urn", $urn);
+    }
+
+    return $flcItem
+}
+
+Write-Host "Create or Update FLC item..."
+$properties = GetFlcProperties "Item Field Mapping" $file
+$flcItem = CreateOrUpdateFLCItem $file $properties
+
+Write-Host "Uploading attachments..."
+$fileAttachments = @()
+
+if ($workflow.Settings.'Upload File Attachments') {
+    $fileAttachments += Get-VaultFileAssociations -File $file._FullPath -Attachments
+}
+
+#TODO: supported file extensions!
+if ($workflow.Settings.'Upload Native Files') {
+    $fileAttachments += $file
+}
+
+$downloadDirectory = Join-Path -Path "C:\Temp\" -ChildPath ([Guid]::NewGuid())
+$uploadedFiles = @()
+$uploadJobs = @()
+foreach ($fileAttachment in $fileAttachments) {
+    if (-not $uploadedFiles.Contains($fileAttachment._FullPath)) {
+        Write-Host "Uploading '$($fileAttachment._Name)'..."
+        $downloadedFiles = Save-VaultFile -File $fileAttachment._FullPath -DownloadDirectory $downloadDirectory
+        $file = $downloadedFiles | Select-Object -First 1
+
+        $paramUpload = [Hashtable]::Synchronized(@{})
+        $paramUpload.FlcItem = $flcItem
+        $paramUpload.LocalPath = $file.LocalPath
+        $paramUpload.Description = $file._Description
+        $uploadJobs += {
+            param ($flcConnection, [Hashtable]$parameters)
+            $fileName = Split-Path $parameters.LocalPath -leaf  
+            $parameters.FlcItem | Add-FLCAttachment -Path $parameters.LocalPath -Title $fileName -Description $parameters.Description
+        } | InvokeAsync -Parameters $paramUpload
+
+        $uploadedFiles += $file._FullPath
+    }
+}
+WaitAll -Jobs $uploadJobs
+
+# Cleanup
+if (Test-Path $downloadDirectory) {
+    Clean-Up -folder $downloadDirectory
+}
+
+$end = [DateTime]::UtcNow
+Write-Host "Completed job '$($job.Name)' in $([int]([TimeSpan]($end - $begin)).TotalSeconds) Seconds"
---
+++ coolorange.flc.transfer.file/README.md
@@ -0,0 +1,61 @@
+# File Publishing Workflow
+
+## Description
+The File Publishing workflow can be used to transfer file metadata and file attachments from Vault to Fusion Lifecycle. The workflow is seamlessly integrated into Vault workflows, triggered by a Vault file state transition and executed on the Autodesk Vault Job Processor.
+
+The entire workflow is based on PowerShell scripts and can be customized if needed.
+
+## Prerequisites
+The option **Enable Job Server** must be set in the **Job Server Management** in Vault and the installation explained below must be executed on a Job Processor machine.  
+The coolOrange products "powerJobs Processor" and "powerFLC" must be installed on the Job Processor machine. Both products can be downloaded from http://download.coolorange.com. 
+
+## Workflow Installation
+- Copy the files located in Jobs and Modules to “C:\ProgramData\coolOrange\powerJobs”
+- In Vault, open the “**powerFLC Configuration Manager**” from the tools menu
+- Import the workflow “**coolorange.flc.transfer.file.json**” using the "Import" button
+- Once imported, double-click the workflow to adjust the settings
+
+## Settings
+### Workspace and Unique Fields
+The selected **Workspace** is used to transfer item and BOM data from Vault to Fusion Lifecycle. A field from this workspace must be chosen as Unique Identifier. Typically, this field represents the document name. The unique Vault Property defaults to **File Name** and should not be changed.
+
+### Workflow Settings
+![image](https://user-images.githubusercontent.com/5640189/101505267-752ff980-3974-11eb-9db3-250ee5f1a1ab.png)
+
+#### Upload File Attachments
+If set to "True" all files attached to a file in Vault are be uploaded as attachment to the corresponding Fusion Lifecycle item
+
+#### Upload Native Files
+If set to "True" the native file is transferred to Fusion Lifecycle.
+
+#### Supported File Extensions
+The extensions for the native files that are allowed to be transferred to Fusion Lifecycle. Only applicable if **Upload Native Files** is set to "True"  
+*Note: the supported file extension must be specified as a single string, seperated by semi-colons*
+
+### Field Mappings  
+An "Item Field Mapping" is available. Values from the file properties chosen in **Vault File Property** column will be copied to the Fusion Lifecycle fields chosen in the **Fusion Lifecycle Item Field** column when an item is created or updated in Fusion Lifecycle.  
+
+## Job Trigger
+### Trigger job on Vault file state change
+In order to configure the workflow to be executed on a Vault lifecycle state transition, Vault's **Custom Job Types** functionality can be used.
+
+1) Log in to Vault as Administrator
+2) Open the Vault settings
+3) Navigate to Bahaviors, Lifecycle...
+4) Edit a "Lifecyle Definition" (e.g. "Flexible Release Process")
+5) Navigate to the "Transitions" tab
+6) Edit the Transition that should trigger the workflow
+7) Navigate to the "Custom Job Types" tab  
+![image](https://user-images.githubusercontent.com/5640189/101505033-2f733100-3974-11eb-8ae2-b1b18516a791.png)
+8) Use the "Add..." command to add the job type "**coolorange.flc.transfer.file**"
+9) Commit the changes using the "OK" button
+
+## Activate the Workflow
+Start / restart powerJobs Processor to automatically register the jobs to Vault's JobProcessor 
+
+## Product documentation
+powerFLC: https://www.coolorange.com/wiki/doku.php?id=powerflc  
+powerJobs Processor: https://www.coolorange.com/wiki/doku.php?id=powerjobs_processor  
+
+## At your own risk
+The usage of these samples is at your own risk. There is no free support related to the samples. However, if you have questions to powerJobs or powerFLC, then visit http://www.coolorange.com/wiki or start a conversation in our support forum at http://support.coolorange.com/support/discussions
